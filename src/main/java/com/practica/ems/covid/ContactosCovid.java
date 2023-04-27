f9c7da4b53dde059156d41a01fe39362d79824d2
Sebastian Yarzabal <s.yarzabal@alumnos.upm.es>
2023-04-27 18:33:22 +0200

    Simplified switch on crearPosicionPersona(...)

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -276,30 +276,16 @@ public class ContactosCovid {
 
 	private PosicionPersona crearPosicionPersona(String[] data) {
 		PosicionPersona posicionPersona = new PosicionPersona();
-		String fecha = null, hora;
-		float latitud = 0, longitud;
-		for (int i = 1; i < Constantes.MAX_DATOS_LOCALIZACION; i++) {
-			String s = data[i];
-			switch (i) {
-			case 1:
-				posicionPersona.setDocumento(s);
-				break;
-			case 2:
-				fecha = data[i];
-				break;
-			case 3:
-				hora = data[i];
-				posicionPersona.setFechaPosicion(parsearFecha(fecha, hora));
-				break;
-			case 4:
-				latitud = Float.parseFloat(s);
-				break;
-			case 5:
-				longitud = Float.parseFloat(s);
-				posicionPersona.setCoordenada(new Coordenada(latitud, longitud));
-				break;
-			}
-		}
+
+		posicionPersona.setDocumento(data[1]);
+		String fecha = data[2];
+		String hora = data[3];
+		posicionPersona.setFechaPosicion(parsearFecha(fecha, hora));
+		float latitud = Float.parseFloat(data[4]);
+		float longitud = Float.parseFloat(data[5]);
+		posicionPersona.setCoordenada(new Coordenada(latitud, longitud));
+
+
 		return posicionPersona;
 	}
 	
